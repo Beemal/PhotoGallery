a082468128136755a0c7d1d065ea370b222ec6a7
Bimal Parajuli <bimalparajuli@Bimals-MacBook-Pro.local>
2017-09-25 23:15:44 -0700

    add image-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { AlertModule } from 'ng2-bootstrap';
+
+import {NavbarComponent} from './navbar/navbar.component';
+import {GalleryComponent} from './gallery/gallery.component';
+
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, NavbarComponent, GalleryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, HttpModule, AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/image/shared/image.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ImageService{
+
+}
+
+const IMAGES = [
+    {"id": 1, "category":"boats", "caption":"view from a boat", "url":"assets/img/boat_01.jpeg"},
+    {"id": 2, "category":"boats", "caption":"sailing the coast", "url":"assets/img/boat_02.jpeg"}
+]
